dc0e74da3f4487cc906744820c5b04d55b8547a0
Tien <1551010131tien@gmail.com>
2017-09-20 11:07:08 +0700

    Ghi chu

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿
+IF OBJECT_ID('uspCau1','P') IS NOT NULL
+	DROP PROC uspCau1
+GO
+
+CREATE PROC uspCau1
+	@a float = 0,
+	@b float = 0,
+	@tong float = 0 OUT
+AS
+	--RETURN @a + @b
+	SET @tong = @a + @b
+GO
+
+--Chay
+DECLARE @tong float ,@x float = 4.5, @y float = 2.3
+EXEC  uspCau1 @x, @y, @tong OUT
+PRINT N'Tổng hai số ' + CAST(@x AS nvarchar) + ' và ' + CAST(@y AS nvarchar) + ' là: ' + CAST(@tong AS nvarchar) 
